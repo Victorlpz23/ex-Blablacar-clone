3e3b2de9413c54ef24257da92a0af8a36ba6c88c
Victor Lopez <victorlpz2301@gmail.com>
2023-02-14 20:43:27 +0100

    tripsMVC

--- configs/routes.config.js
+++ configs/routes.config.js
@@ -4,9 +4,12 @@ const router = express.Router();
 
 // You will need a constant for use actions defined inside the controller files
 const commonsController = require('../controllers/commons.controller');
+const tripsController= require('../controllers/trips.controller')
 
 // Defining the actions for paht request
 router.get('/', commonsController.home);
+
+router.get('/trips', tripsController.list);
 
 
 
---
+++ controllers/trips.controller.js
@@ -0,0 +1,7 @@
+
+
+// Defining actions for trips
+
+module.exports.list = (req, res, next) => {
+  res.render('trips/list')
+}
