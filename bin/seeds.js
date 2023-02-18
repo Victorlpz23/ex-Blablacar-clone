d30f17e654133360098c86fdb5ef158e67f27c2e
Victor Lopez <victorlpz2301@gmail.com>
2023-02-18 13:16:49 +0100

    Creating a seedwith users and trips

--- bin/seeds.js
+++ bin/seeds.js
@@ -5,22 +5,34 @@ require('../configs/db.config');
 // Requiring model to create the seed
 const Trip = require('../models/trip.model');
 
+// Requiring model to create the seed
+const User = require('../models/user.model');
 
-// Creating the seed with the model keys
-Trip.deleteMany()
+
+// Creating the seed with the User & trip
+User.deleteMany()
 .then(() => {
   for (let i = 0; i <= 10; i++) {
-    Trip.create({
-      user: `User ${i}`,
-      from: `City from ${i}`,
-      to: `City of destiny ${i}`,
-      price: 30,
-      date: `Date ${i}`,
-      seats: 3,
-      comments: `Comment ${i}`,
-    }).then((trip) => {
-      console.log(`trip ${i} created`)
-    }).catch((error) => console.error(error))
+    User.create({
+      user: `name${i}`,
+      email: `email${i}@seeds.com`,
+      password: `12345678`,
+    })
+    .then(user => {
+        Trip.create({
+          user: user.id,
+          from: `City from ${i}`,
+          to: `City of destiny ${i}`,
+          price: 30,
+          date: `Date ${i}`,
+          seats: 3,
+          comments: `Comment ${i}`,
+        }).then((trip) => {
+          console.log(`trip ${i} created`)
+        }).catch((error) => console.error(error))
+      }
+    ).catch((error) => console.error(error))
   }
 });
+
 
