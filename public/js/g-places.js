27495ba41bbf534addb354242a8663aa11b1dbfc
Victor Lopez <victorlpz2301@gmail.com>
2023-03-07 11:10:38 +0100

    Map view for create a trip

--- public/js/g-places.js
+++ public/js/g-places.js
@@ -3,8 +3,38 @@
 function initGooglePlaces() {
   console.log('Google places script loaded')
   initSearchBar()
+  initGMap()
 }
 
+// Initialize Google Map
+function initGMap() {
+  const mapContainer = document.querySelector('.g-map');
+  if (mapContainer) {
+  const latFrom = document.getElementById('latFrom')
+  const valueLatFrom = latFrom.getAttribute("value")
+  const lngFrom = document.getElementById('lngFrom')
+  const valueLngFrom = lngFrom.getAttribute("value")
+  let center 
+  let zoomValue = 4
+  console.log(valueLatFrom)
+    if (valueLatFrom && valueLngFrom) {
+      center = { lat: +valueLatFrom, lng: +valueLngFrom}
+      zoomValue = 12
+    } else {
+      center = { lat: 40.4167754, lng: -3.7037902 }
+    }
+    const map = new google.maps.Map(mapContainer, {
+      zoom: zoomValue,
+      center: center,
+    });
+    const marker = new google.maps.Marker({
+      position : center,
+      map: map,
+    });
+  } 
+}
+
+// Initialize Google search bar
 function initSearchBar() {
   const input = document.querySelectorAll('.g-places-finder');
   if (input) {
@@ -24,8 +54,8 @@ function initSearchBar() {
 
       document.querySelector(`[name="lat${destination}"]`).value = lat;
       document.querySelector(`[name="lng${destination}"]`).value = lng;
+      initGMap()
     });
     })
-    
   }
 }
